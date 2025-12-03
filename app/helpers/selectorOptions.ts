cd9cfddfe163b3f3752ae1c42e9f2476498fb00c
Dmytro_Gunko <dimagunko99@gmail.com>
2025-12-03 16:28:26 +0100

    changed choose a price fild label

--- app/helpers/selectorOptions.ts
+++ app/helpers/selectorOptions.ts
@@ -1,12 +1,11 @@
-
 export type Option = {
   value: number;
   label: string;
 };
 export const carPriceOptions: Option[] = [
-  { value: 40, label: 'To $40' },
-  { value: 50, label: 'To $50' },
-  { value: 60, label: 'To $60' },
-  { value: 70, label: 'To $70' },
-  { value: 80, label: 'To $80' },
+  { value: 40, label: '40' },
+  { value: 50, label: '50' },
+  { value: 60, label: '60' },
+  { value: 70, label: '70' },
+  { value: 80, label: '80' },
 ];
